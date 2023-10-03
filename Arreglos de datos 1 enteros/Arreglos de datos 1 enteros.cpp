b2cbbe1b73207f4017e54169dc521aa63b245cb2
Alex5 <Alex5@TOLEDO521>
2023-10-02 18:07:46 -0600

    Agregar archivos de proyecto.

---
+++ Arreglos de datos 1 enteros/Arreglos de datos 1 enteros.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main() {
+	int numeros[6] = { 10, 20, 30, 40, 50, 60 };
+
+	std::cout << "el numero que eligio es: " << numeros[5] << std::endl;
+
+
+	system("pause");
+	return 0;
+}
